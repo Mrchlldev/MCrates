d6059eb756ddcb0325e7290e23e99ace14c9f2d0
MyFreds <82293397+MyFreds@users.noreply.github.com>
2024-08-17 12:19:05 +0800

    Lint

--- src/Mrchlldev/MCrates/MCrates.php
+++ src/Mrchlldev/MCrates/MCrates.php
@@ -100,7 +100,7 @@ class MCrates extends PluginBase
                 try {
                     $item = StringToItemParser::getInstance()->parse($itemData["item"]) ?? LegacyStringToItemParser::getInstance()->parse($itemData["item"]) ?? CustomiesItemFactory::getInstance()->get($itemData["item"]);
                 }catch(LegacyStringToItemParserException $e){
-                    echo $e->getMessage();
+                    $this->getLogger()->warning($e->getMessage());
                 }
                 if($tags !== null) {
                     $item->setNamedTag($tags);
